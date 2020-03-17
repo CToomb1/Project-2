d71be6f9dbcdaa81471fc2d7119dd80f5ca2283a
Conor Toomb <conor.toomb114@gmail.com>
2020-03-17 14:32:52 -0400

    improved on sql some

--- db/schedule.sql
+++ db/schedule.sql
@@ -2,14 +2,14 @@ CREATE TABLE employees
 (
     id INT NOT NULL
     AUTO_INCREMENT,
-  burger_name VARCHAR
+  empName VARCHAR
     (255) NOT NULL,
   devoured BOOLEAN DEFAULT false NOT NULL,
   PRIMARY KEY
     (id)
 );
     INSERT INTO employees
-        (employee_name)
+        (empName)
     VALUES
     ('Housekeeping', 3, 000.00, 1);
     ('Room Service', 3,000.00, 2);
