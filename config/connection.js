4553b10097b877f994df3c307e3813d1d9aa2153
Conor Toomb <conor.toomb114@gmail.com>
2020-03-20 17:39:27 -0400

    added dropdown button, menu not working

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 
+const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
 // create connection to our db
@@ -7,7 +9,6 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      dialect: "mysql",
       port: 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PW,
