f5b1a1d4c7cd8bad3713392c3827e461e1454c67
Conor Toomb <conor.toomb114@gmail.com>
2020-03-20 14:56:49 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     
       host: 'localhost',
+      dialect: "mysql",
       port: 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PW,
